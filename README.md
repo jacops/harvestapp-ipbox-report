1bfd80781c1be2dd327568b0b9ac82b5d0c1a59b
Jakub Igla <jakub.igla@getindata.com>
2022-12-23 14:38:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,34 +1,13 @@
-# python-cli-template
+# HarvestApp IP Box Rpoert
 
-A starting point for building Python Command Line Applications.
+This simple script will help you create a yearly report for your `IP Box` tax relief from the HarvestApp entries.
 
-## About 
+## Usage
 
-This project serves as a starting point to developing command line modules with Python. It is structured in such a way that 
-when we call the module it executes the main method in `app/cli.py`. This is typically where you would want to add 
-your own logic.
+1. Create `.env` file from the `.env.dist` file.
+2. Fill the `.env` file with the authentication credentials - https://id.getharvest.com/developers
+3. Install python dependencies via `pipenv`: `pipenv install`
+4. Run the script: `pipenv run python runner.py 2022` or simply `make run YEAR=2022`
 
-The setup.py file includes some advanced patterns and best 
-practices for setup.py, as well as some commented–out nice–to–haves. For example, it provides a `python 
-setup.py upload` command, which creates a universal wheel (and sdist) and uploads your package to PyPi using Twine. 
-It also creates/uploads a new git tag, automatically.
-
-## Setup
-
-```
-virtualenv env
-source env/bin/activate
-pip install -r requirements.txt
-python -m app --help
-```
-
-## Packaging 
-
-Update `setup.py` with your details and then run `python setup.py upload` to package for distribution on PyPi.
-
-## Contributing
-
-- Fork the project and clone locally.
-- Create a new branch for what you're going to work on.
-- Push to your origin repository.
-- Create a new pull request in GitHub.
+> The `HARVEST_IPBOXABLE_TASK_PHRASES` environmental variables takes comma separated values. 
+> Any of the provided phrases needs to be found in the Harvest task name in order to mark it as "ipboxable".
--- app/cli.py
+++ app/cli.py
@@ -1,12 +1,63 @@
+import csv
+import datetime
+import os
+import json
+import urllib.request
 from argparse import ArgumentParser
-
+from dotenv import load_dotenv
 
 def main():
     parser = ArgumentParser(prog='cli')
-    parser.add_argument('name', help="The user's name.")
+    parser.add_argument('year', help="Year")
     args = parser.parse_args()
-    print("Hello, %s!" % args.name)
 
+    ipboxable_task_phrases = os.environ["HARVEST_IPBOXABLE_TASK_PHRASES"].split(",")
+
+    year = args.year
+    calendar = get_calendar(year)
+
+    url = f"https://api.harvestapp.com/v2/time_entries?from={year}-01-01&to={year}-12-31"
+
+    response = harvest_request(url=url)
+
+    for entry in response["time_entries"]:
+        if any(map(entry["task"]["name"].__contains__, ipboxable_task_phrases)):
+            calendar[entry["spent_date"]]["ipboxable"] += entry["hours"]
+        calendar[entry["spent_date"]]["hours"] += entry["hours"]
+
+    file_name = f"report_{year}.csv"
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["year", "hours", "ipboxable"])
+        for date in calendar:
+            writer.writerow([date, calendar[date]["hours"], calendar[date]["ipboxable"]])
+
+    print(f"Report generated in {file_name}...")
+
+def harvest_request(url):
+    load_dotenv()
+    headers = {
+        "User-Agent": "Python Harvest API Sample",
+        "Authorization": "Bearer " + os.environ.get("HARVEST_ACCESS_TOKEN"),
+        "Harvest-Account-ID": os.environ.get("HARVEST_ACCOUNT_ID")
+    }
+    request = urllib.request.Request(url=url, headers=headers)
+    response = urllib.request.urlopen(request, timeout=5)
+    responseBody = response.read().decode("utf-8")
+
+    return json.loads(responseBody)
+
+def get_calendar(year):
+    next_year = int(year) + 1
+    start = datetime.datetime.strptime(f"01-01-{year}", "%d-%m-%Y")
+    end = datetime.datetime.strptime(f"01-01-{next_year}", "%d-%m-%Y")
+    date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+
+    calendar = {}
+    for date in date_generated:
+        calendar[date.strftime("%Y-%m-%d")] = {"hours": 0, "ipboxable": 0}
+
+    return calendar
 
 if __name__ == '__main__':
     main()
